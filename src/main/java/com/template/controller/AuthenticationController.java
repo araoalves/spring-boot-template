47b643d2fb91ffb1284695d4c53c5b5dca321594
Felipe Goiabeira <felipe.goiabeira@ceuma.br>
2020-11-23 10:10:55 -0300

    Adicionando tratamento de exceções

--- src/main/java/com/template/controller/AuthenticationController.java
+++ src/main/java/com/template/controller/AuthenticationController.java
@@ -52,7 +52,7 @@ public class AuthenticationController {
 		} catch (DisabledException e) {
 			throw new Exception("USER_DISABLED", e);
 		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
+			throw new BadCredentialsException("INVALID_CREDENTIALS", e);
 		}
 	}
 }
