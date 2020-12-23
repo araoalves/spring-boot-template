542d0a2889a150ecf8a9c9b1a50a8ea2a7ce66f1
Felipe Goiabeira <felipe.goiabeira@ceuma.br>
2020-12-23 12:34:50 -0300

    Modularizando jobs com spring profiles

--- src/main/java/com/template/job/Job.java
+++ src/main/java/com/template/job/Job.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
+@Profile("job")
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduling.enable", matchIfMissing = true)
 public class Job {
